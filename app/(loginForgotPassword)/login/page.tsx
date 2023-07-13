bbb3beea045e7debfa8f36b572d7dac2cf41cc0d
sohanemon <m.sohanemon@gmail.com>
2023-07-13 18:18:45 +0600

    feat: icon input

--- app/(loginForgotPassword)/login/page.tsx
+++ app/(loginForgotPassword)/login/page.tsx
@@ -1,4 +1,6 @@
+import { InputWithIcon } from '@/components/atoms/Input';
 import { Text } from '@/components/atoms/Text';
+import MailIcon from '@mui/icons-material/Mail';
 
 const Login = () => {
   return (
@@ -7,6 +9,11 @@ const Login = () => {
       <Text color={'muted.main'} variant='body2'>
         Enter your credentials to continue
       </Text>
+      <InputWithIcon
+        label='Email Address'
+        icon={MailIcon}
+        placeholder='demo@devias.io'
+      />
     </div>
   );
 };
--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -1,7 +1,41 @@
 'use client';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { IconProps, SvgIconTypeMap } from '@mui/material';
+import { InputProps } from '@mui/material/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+import { useState } from 'react';
 
-import { TextField, InputProps } from '@mui/material';
-
-export default function Input(props: InputProps) {
-  return <TextField sx={{ height: 47 }} {...props} />;
+interface InputWithIconProps {
+  icon: OverridableComponent<SvgIconTypeMap<{}, 'svg'>>;
+  placeholder: string;
+  label: string;
+}
+export function InputWithIcon(props: InputWithIconProps) {
+  const [focused, setFocused] = useState(false);
+  function Icon({
+    icon: Icon,
+  }: {
+    icon: OverridableComponent<SvgIconTypeMap<{}, 'svg'>>;
+  }) {
+    return <Icon color={(focused ? 'primary' : '') as any} />;
+  }
+  return (
+    <TextField
+      fullWidth
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      label={focused ? props.label?.toString() : null}
+      placeholder={focused ? props.placeholder : props.label?.toString()}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position='start'>
+            <Icon icon={props.icon} />
+          </InputAdornment>
+        ),
+      }}
+      variant='outlined'
+    />
+  );
 }

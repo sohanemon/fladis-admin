03943280e7d6d4ea47515c5807e8c5dc66cbe957
sohanemon <m.sohanemon@gmail.com>
2023-07-21 12:00:23 +0600

    feat: timestamp

--- components/atoms/notifications.tsx
+++ components/atoms/notifications.tsx
@@ -1,5 +1,5 @@
 import { Avatar } from '@mui/material';
-
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 export default function Notifications() {
   return (
     <section className='notification__notifications'>
@@ -10,7 +10,12 @@ export default function Notifications() {
           Lorem ipsum dolor sit amet consectetur. Et curabitur dui suspendisse
           magna.
         </p>
-        <div className='timestamp'></div>
+        <div className='timestamp'>
+          <AccessTimeFilledIcon
+            sx={{ color: '#CCC', width: '12px', height: '12px' }}
+          />
+          <p>2 days ago</p>
+        </div>
       </div>
     </section>
   );

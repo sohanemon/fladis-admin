a8314bc0f7508d0a37a0bceb2069cc442d57af42
sohanemon <m.sohanemon@gmail.com>
2023-07-26 21:33:16 +0600

    feat: tabs

--- components/molecules/add-user-form.tsx
+++ components/molecules/add-user-form.tsx
@@ -10,94 +10,98 @@ import { useRef } from 'react';
 import { InputWithIcon } from '../atoms/Input';
 import { Select } from '../atoms/select';
 import { Button } from '../atoms/Button';
+import { Tabs } from './tabs';
 
 export default function AddUserForm({ edit }: { edit?: boolean }) {
   const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <Box
-      padding={'14px 22px'}
-      sx={{
-        borderRadius: '5px',
-        background: '#FFF',
-        boxShadow: '0px 4px 16px 0px rgba(0, 0, 0, 0.10)',
-      }}
-    >
-      {edit ? (
-        <br />
-      ) : (
-        <>
-          <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>
-            User information
-          </Typography>
-          <Typography
-            sx={{ color: '#A9AAAD', m: '0.5rem 0', fontSize: '12px' }}
-          >
-            Profile Picture
-          </Typography>
-        </>
-      )}
+    <>
+      <Tabs tabs={tabs} />
       <Box
-        onClick={() => inputRef.current?.click()}
+        padding={'14px 22px'}
         sx={{
-          position: 'relative',
-          width: '108px',
-          marginInline: 'auto',
-          cursor: 'pointer',
-          aspectRatio: '1/1',
-          display: 'grid',
-          placeContent: 'center',
-          bgcolor: '#5E008420',
-          borderRadius: 999,
-          outline: '1px dashed #E2E2E2',
-          outlineOffset: 9,
+          borderRadius: '5px',
+          background: '#FFF',
+          boxShadow: '0px 4px 16px 0px rgba(0, 0, 0, 0.10)',
         }}
       >
-        <PhotoSizeSelectActualIcon sx={{ filter: 'invert()' }} />
-        <Box sx={{ position: 'absolute', right: 0, bottom: -14 }}>
-          <Image
-            src={`/assets/images/user/add.svg`}
-            alt=''
-            width={21}
-            height={21}
-          />
-        </Box>
-        <input ref={inputRef} type='file' style={{ display: 'none' }} />
-      </Box>
-      <Box
-        display={'grid'}
-        mx={'auto'}
-        maxWidth={['100%', '80%']}
-        gridTemplateColumns={'1fr 1fr'}
-        gap={[1, 4]}
-        my={6}
-      >
-        {inputFields.map((_) => (
-          <InputWithIcon {..._} key={_.label} />
-        ))}
-        <Select label='Gender' />
-        <Select label='Role' />
-      </Box>
-      <Stack
-        direction={'row'}
-        borderTop={'1px solid #C3C3C520'}
-        pt={3}
-        justifyContent={'end'}
-        mx={'auto'}
-        gap='1rem'
-        maxWidth={['100%', '80%']}
-      >
         {edit ? (
-          <Button variant='contained'>Save Changes</Button>
+          <br />
         ) : (
           <>
-            <Button color='info' variant='contained'>
-              Back
-            </Button>
-            <Button variant='contained'>Submit</Button>
+            <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>
+              User information
+            </Typography>
+            <Typography
+              sx={{ color: '#A9AAAD', m: '0.5rem 0', fontSize: '12px' }}
+            >
+              Profile Picture
+            </Typography>
           </>
         )}
-      </Stack>
-    </Box>
+        <Box
+          onClick={() => inputRef.current?.click()}
+          sx={{
+            position: 'relative',
+            width: '108px',
+            marginInline: 'auto',
+            cursor: 'pointer',
+            aspectRatio: '1/1',
+            display: 'grid',
+            placeContent: 'center',
+            bgcolor: '#5E008420',
+            borderRadius: 999,
+            outline: '1px dashed #E2E2E2',
+            outlineOffset: 9,
+          }}
+        >
+          <PhotoSizeSelectActualIcon sx={{ filter: 'invert()' }} />
+          <Box sx={{ position: 'absolute', right: 0, bottom: -14 }}>
+            <Image
+              src={`/assets/images/user/add.svg`}
+              alt=''
+              width={21}
+              height={21}
+            />
+          </Box>
+          <input ref={inputRef} type='file' style={{ display: 'none' }} />
+        </Box>
+        <Box
+          display={'grid'}
+          mx={'auto'}
+          maxWidth={['100%', '80%']}
+          gridTemplateColumns={'1fr 1fr'}
+          gap={[1, 4]}
+          my={6}
+        >
+          {inputFields.map((_) => (
+            <InputWithIcon {..._} key={_.label} />
+          ))}
+          <Select label='Gender' />
+          <Select label='Role' />
+        </Box>
+        <Stack
+          direction={'row'}
+          borderTop={'1px solid #C3C3C520'}
+          pt={3}
+          justifyContent={'end'}
+          mx={'auto'}
+          gap='1rem'
+          maxWidth={['100%', '80%']}
+        >
+          {edit ? (
+            <Button variant='contained'>Save Changes</Button>
+          ) : (
+            <>
+              <Button color='info' variant='contained'>
+                Back
+              </Button>
+              <Button variant='contained'>Submit</Button>
+            </>
+          )}
+        </Stack>
+      </Box>
+    </>
   );
 }
 
@@ -111,4 +115,15 @@ const inputFields = [
   },
   { label: 'Phone', icon: LocalPhoneIcon, type: 'number' },
   { label: 'Email', icon: EmailIcon, type: 'email' },
+];
+
+const tabs = [
+  {
+    label: 'edit details',
+    link: '/dashboard/administration/users',
+  },
+  {
+    label: 'change password',
+    link: '/dashboard/administration/users/change-password',
+  },
 ];
---
+++ components/molecules/tabs.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { Box, BoxProps } from '@mui/material';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { forwardRef } from 'react';
+
+interface TabsProps extends BoxProps {
+  tabs: { label: string; link?: string }[];
+}
+
+const Tabs = forwardRef<BoxProps, TabsProps>(({ tabs, ...props }, ref) => {
+  const path = usePathname();
+  return (
+    <Box
+      ref={ref}
+      {...props}
+      sx={[
+        {
+          borderRadius: '5px',
+          display: 'flex',
+          px: '21px',
+          mb: 3,
+          gap: 4,
+          background: 'white',
+          boxShadow: '0px 4px 16px rgba(0, 0, 0, 0.10)',
+        },
+        ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
+      ]}
+    >
+      {tabs.map((_) => (
+        <Link
+          style={{
+            color: path.includes(_.link!) ? '#5E0084' : '#A5A5A5',
+            fontSize: 14,
+            display: 'block',
+            paddingTop: '10px',
+            paddingBottom: '6px',
+            fontWeight: 500,
+            textTransform: 'capitalize',
+            borderBottom: '4px solid',
+            borderColor: path.includes(_.link!) ? '#5E0084' : 'transparent',
+          }}
+          key={_.label}
+          href={_.link!}
+        >
+          {_.label}
+        </Link>
+      ))}
+    </Box>
+  );
+});
+
+Tabs.displayName = 'Tabs';
+export { Tabs };

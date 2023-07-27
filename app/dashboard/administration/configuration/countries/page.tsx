1bdfce9dcd69c99a65e914926dbd26f6e1c0ca46
sohanemon <m.sohanemon@gmail.com>
2023-07-27 15:46:16 +0600

    feat: countries

--- app/dashboard/administration/configuration/countries/page.tsx
+++ app/dashboard/administration/configuration/countries/page.tsx
@@ -1,10 +1,45 @@
-import React from "react";
+'use client';
+import { FilledButton } from '@/components/atoms/Button';
+import { FilledInput } from '@/components/atoms/Input';
+import { GiftModal } from '@/components/modals/gift-modal';
+import CountryTable from '@/components/molecules/configuration-tables/countries-table';
+import VatTable from '@/components/molecules/configuration-tables/vat-table';
+import { useModalStore } from '@/zustand-store/Modal.store';
+import AddIcon from '@mui/icons-material/Add';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import { Box, Stack } from '@mui/material';
 
 const Countries = () => {
+  const { setModalContent, setModalShow } = useModalStore();
+
+  function handleAdd() {
+    setModalShow(true);
+    setModalContent(<GiftModal setModalShow={setModalShow}></GiftModal>);
+  }
   return (
-    <div>
-      <p>Countries</p>
-    </div>
+    <Box>
+      <Stack direction={'row'} justifyContent={'space-between'}>
+        <div className='heading'>
+          <h1>Countries</h1>
+          <p>
+            Configuration
+            <FiberManualRecordIcon sx={{ width: 6, fill: '#A5A5A5' }} />
+            <span>Countries</span>
+          </p>
+        </div>
+        <Stack direction={'row'} gap={[1, 2]}>
+          <FilledInput placeholder='Search' />
+          <FilledButton
+            onClick={handleAdd}
+            primary
+            icon={<AddIcon sx={{ fontSize: 20 }} />}
+          >
+            Add
+          </FilledButton>
+        </Stack>
+      </Stack>
+      <CountryTable />
+    </Box>
   );
 };
 
